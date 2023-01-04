761af1427264a3b7db138bb0db4ea7a5d2f96e7e
gamal ahmed <gamalahmed3265@gmail.com>
2023-01-04 13:57:00 +0200

    end the appbar

--- lib/views/widget/custom_view_body.dart
+++ lib/views/widget/custom_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/resource/app_size.dart';
+import 'package:note_app/views/widget/custom.card_note.dart';
 import 'package:note_app/views/widget/custom_app_bar.dart';
 
 class CustomViewBody extends StatelessWidget {
@@ -6,19 +8,11 @@ class CustomViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [CustomAppBar(), CardNote()],
-    );
-  }
-}
-
-class CardNote extends StatelessWidget {
-  const CardNote({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Text("dfmdf"),
+    return Padding(
+      padding: const EdgeInsets.all(AppSize.s8),
+      child: Column(
+        children: const [CustomAppBar(), CardNote()],
+      ),
     );
   }
 }
