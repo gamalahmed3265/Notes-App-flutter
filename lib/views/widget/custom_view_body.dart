959d799dc83f3b342007dfadcace6df4f34e6c40
gamal ahmed <gamalahmed3265@gmail.com>
2023-01-04 17:41:48 +0200

    add the note list view

--- lib/views/widget/custom_view_body.dart
+++ lib/views/widget/custom_view_body.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:note_app/data/local/data_note.dart';
-import 'package:note_app/domain/model/note_data_model.dart';
 import 'package:note_app/views/resource/app_size.dart';
-import 'package:note_app/views/widget/custom_note_item.dart';
 import 'package:note_app/views/widget/custom_app_bar.dart';
+import 'package:note_app/views/widget/note_list_view.dart';
 
 class CustomViewBody extends StatelessWidget {
   const CustomViewBody({Key? key}) : super(key: key);
@@ -13,17 +11,12 @@ class CustomViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: AppSize.s24),
       child: Column(
-        children: [
-          const SizedBox(
+        children: const [
+          SizedBox(
             height: AppSize.s50,
           ),
-          const CustomAppBar(),
-          const SizedBox(height: AppSize.s16),
-          NoteItem(noteDataModel: listNote[0]),
-          const SizedBox(height: AppSize.s16),
-          NoteItem(noteDataModel: listNote[1]),
-          const SizedBox(height: AppSize.s16),
-          NoteItem(noteDataModel: listNote[2])
+          CustomAppBar(),
+          Expanded(child: NoteListView())
         ],
       ),
     );
---
+++ lib/views/widget/note_list_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/data/local/data_note.dart';
+import 'package:note_app/views/resource/app_size.dart';
+import 'package:note_app/views/widget/custom_note_item.dart';
+
+class NoteListView extends StatelessWidget {
+  const NoteListView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+        itemBuilder: (context, index) => const SizedBox(height: AppSize.s16),
+        separatorBuilder: (context, index) => InkWell(
+              child: NoteItem(noteDataModel: listNote[index]),
+              onTap: () {
+                print(index);
+              },
+            ),
+        itemCount: listNote.length);
+  }
+}
